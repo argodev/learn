27219d59691f1274620b7be97617206ebd5bf886
Rob Gillen <gillenre@ornl.gov>
2018-01-26 14:11:02 -0500

    Updating v2 for large memory

--- books/cuda_engineers/Appendix_C/dist_v2/main.cpp
+++ books/cuda_engineers/Appendix_C/dist_v2/main.cpp
@@ -1,11 +1,16 @@
 #include "aux_functions.h"
-#define N 64
+#include <stdlib.h>         // supports dynamic memory management
+// #define N 64
+#define N 20000000 // a large array size
 
 int main() {
     // create two arrays of N floats (initialized to 0.0)
     // we will overwrite these values to store inputs and outputs
-    float in[N] = {0.0f};
-    float out[N] = {0.0f};
+    // float in[N] = {0.0f};
+    // float out[N] = {0.0f};
+
+    float *in = (float*)calloc(N, sizeof(float));
+    float *out = (float*)calloc(N, sizeof(float));
 
     // chose a reference value from which distances are measured
     const float ref = 0.5f;
@@ -17,6 +22,10 @@ int main() {
 
     // single function call to compute entire distance array
     distanceArray(out, in, ref, N);
+
+    // release the heap memory after we are done using it
+    free(in);
+    free(out);
 
     return 0;
 }
